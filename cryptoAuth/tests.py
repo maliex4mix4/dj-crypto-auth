a4d4e73844e0db1b83c4d02a4c6889f53d9f0f66
maliex4mix4 <malikopeyemi5@gmail.com>
2021-11-11 03:47:31 +0100

    Done

--- cryptoAuth/tests.py
+++ cryptoAuth/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from .views import confirm_auth_coinbase
+from django.test.client import RequestFactory
+from django.utils import unittest
 
-# Create your tests here.
+
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test_alive(self):
+        # Create an instance of a GET request.
+
+        request = self.factory.get('/coinbase/confirm_auth')
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = confirm_auth_coinbase(request)
+        self.assertEqual(response.status_code, 200)
